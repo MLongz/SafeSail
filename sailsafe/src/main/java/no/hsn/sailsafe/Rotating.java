9a98bbb2fc133c4ea29d9c884dfe1ca88624e4a6
Khest <knut.j.hesten@student.hbv.no>
2016-05-18 18:46:59 +0200

    Added javadoc

--- sailsafe/src/main/java/no/hsn/sailsafe/Rotating.java
+++ sailsafe/src/main/java/no/hsn/sailsafe/Rotating.java
@@ -9,22 +9,35 @@ import org.mapsforge.map.android.graphics.AndroidGraphicFactory;
 import org.mapsforge.map.layer.overlay.Marker;
 
 /**
- * Created by M. Long on 09.03.2016.
+ * Extension of the Marker class to support rotation of drawable objects
+ * Created by Long Hyunh on 09.03.2016.
  */
 public class Rotating extends Marker {
     private float rotation;
 
+    /**
+     *
+     * @param latLong Object containing latitude and longitude coordinates of the rotating object
+     * @param bitmap Bitmap object containing the drawable resource
+     * @param horizontalOffset Horizontal offset of the bitmap object
+     * @param verticalOffset Vertical offset of the bitmap object
+     * @param rotation Degrees rotation to apply to the bitmap object
+     */
     public Rotating(LatLong latLong, Bitmap bitmap, int horizontalOffset,
                     int verticalOffset, float rotation) {
         super(latLong, bitmap, horizontalOffset, verticalOffset);
         this.rotation = rotation;
     }
 
-
+    /**
+     * Draw method override to support rotating of map marker objects
+     * @param boundingBox An area defined by two sets of latitude and longitude coordinates
+     * @param zoomLevel Set zoom level from 1-21 for the drawn item
+     * @param canvas The canvas object to draw the marker on
+     * @param topLeftPoint The screen coordinate of the top leftmost point of the visible area
+     */
     @Override
-    public void draw(BoundingBox boundingBox, byte zoomLevel, org.mapsforge.core.graphics.Canvas canvas, Point topLeftPoint)
-    {
-
+    public void draw(BoundingBox boundingBox, byte zoomLevel, org.mapsforge.core.graphics.Canvas canvas, Point topLeftPoint) {
         android.graphics.Canvas androidCanvas = AndroidGraphicFactory.getCanvas(canvas);
         androidCanvas.save();
 
@@ -37,25 +50,33 @@ public class Rotating extends Marker {
 
         int left = (int) (pixelX - topLeftPoint.x - halfBitmapWidth + this.getHorizontalOffset());
         int top = (int) (pixelY - topLeftPoint.y - halfBitmapHeight + this.getVerticalOffset());
-        //androidCanvas.rotate(rotation, canvas.getWidth()/2, canvas.getHeight()/2);
-        //androidCanvas.rotate(rotation, pixelX, pixelY);
 
         androidCanvas.rotate(getRotation(), (float) (pixelX - topLeftPoint.x), (float) (pixelY - topLeftPoint.y));
         canvas.drawBitmap(this.getBitmap(), left, top);
 
-        //super.draw(boundingBox, zoomLevel, canvas, topLeftPoint);
         androidCanvas.restore();
-
     }
 
+    /**
+     * Gets the current rotation of the drawable object
+     * @return float value representing the rotation in degrees
+     */
     public float getRotation() {
         return rotation;
     }
 
+    /**
+     * Sets the current rotation of the drawable object
+     * @param rotation float value representing the rotation in degrees
+     */
     public void setRotation(float rotation) {
         this.rotation = rotation;
     }
 
+    /**
+     * Sets the current location of the drawable object
+     * @param location The latitude and longitude location of the drawable object
+     */
     public void setLocation(LatLong location) {
         super.setLatLong(location);
     }
